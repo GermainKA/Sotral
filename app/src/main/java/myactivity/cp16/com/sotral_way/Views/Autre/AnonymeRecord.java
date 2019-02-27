de496a0c05d2a2d1245bfe3fa6d0d863b287eed4
shab <alaitoathenas@gmail.com>
2019-02-27 10:34:14 +0000

    secondcommit

--- app/src/main/java/myactivity/cp16/com/sotral_way/Views/Autre/AnonymeRecord.java
+++ app/src/main/java/myactivity/cp16/com/sotral_way/Views/Autre/AnonymeRecord.java
@@ -2,13 +2,33 @@ package myactivity.cp16.com.sotral_way.Views.Autre;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.concurrent.Executor;
+import java.util.concurrent.TimeUnit;
 
 import myactivity.cp16.com.sotral_way.R;
 
@@ -18,6 +38,13 @@ import myactivity.cp16.com.sotral_way.R;
  */
 public class AnonymeRecord extends Fragment {
     Button bntRegisterAnony;
+    EditText numtelephone;
+    EditText adresse;
+
+    FirebaseAuth auth;
+    FirebaseDatabase mdatabase;
+    DatabaseReference mreference;
+
 
 
     public static final String TAG = "AdministrateurFragment";
@@ -27,10 +54,16 @@ public class AnonymeRecord extends Fragment {
         // Inflate the layout for this fragment
          View view =inflater.inflate ( R.layout.fragment_anonyme_record, container, false );
             bntRegisterAnony =(Button)view.findViewById(R.id.idBntRegisterAnony);
+
+        numtelephone= view.findViewById(R.id.input6);
+        adresse = view.findViewById(R.id.input7);
+
             View.OnClickListener bntRegisterListener=new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    bntRegisterAnonyOnClick();
+                    String textnumtelephone=  numtelephone.getText().toString();
+                    String  texttadresse = adresse.getText().toString();
+
 
                 }
             };
@@ -49,4 +82,87 @@ public class AnonymeRecord extends Fragment {
         }
     }
 
+    private void SendtoFirebase(final String numero, final String addresse )
+    {
+        if(TextUtils.isEmpty(numero) ||  TextUtils.isEmpty(addresse)  )
+        {
+            Toast.makeText(getContext(),"champs non rensegné",Toast.LENGTH_SHORT).show();
+        }
+        else if(numero.length()<8)
+        {
+            Toast.makeText(getContext(),"mot de passe doit contenir plus de 6 caractere",Toast.LENGTH_SHORT).show();
+        }
+        else{
+            auth = FirebaseAuth.getInstance();
+
+            PhoneAuthProvider.getInstance().verifyPhoneNumber(numero,60, TimeUnit.SECONDS, getActivity(),phoneAuthCallback);
+
+            /*auth.createUserWithEmailAndPassword(email , password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful())
+                    {
+                        FirebaseUser user = auth.getCurrentUser();
+                        String userid = user.getUid();
+                        mreference = FirebaseDatabase.getInstance().getReference("Etudiants");
+                        HashMap<String,  String> etudiant = new HashMap<>();
+                        etudiant.put("nom", nom);
+                        etudiant.put("prenom",prenom);
+                        etudiant.put("email", email);
+                        etudiant.put("password", password);
+                        etudiant.put("numcard", numcard);
+                        mreference.setValue(etudiant).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Toast.makeText(getContext(),"donnees enregistrer avec succes ",Toast.LENGTH_SHORT).show();
+
+                                bntRegisterAnonyOnClick();
+                            }
+                        });
+
+
+                    }
+
+                }
+            })*/
+
+        }
+    }
+
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks phoneAuthCallback = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+            siginWithPhoneAuthCredencial(phoneAuthCredential);
+
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+
+        }
+    };
+
+    private  void siginWithPhoneAuthCredencial(PhoneAuthCredential phoneAuthCredential)
+    {
+        auth.signInWithCredential(
+                phoneAuthCredential
+        ).addOnCompleteListener( getActivity(),new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful())
+                {
+                    //sign in success message
+                    FirebaseUser user = task.getResult().getUser();
+                }
+                else
+                {
+                    // toast
+                            if(task.getException() instanceof FirebaseAuthInvalidCredentialsException)
+                            {
+
+                            }
+                }
+            }
+        });
+    }
 }
--- app/src/main/java/myactivity/cp16/com/sotral_way/Views/Etudiant/EtudiantRecord.java
+++ app/src/main/java/myactivity/cp16/com/sotral_way/Views/Etudiant/EtudiantRecord.java
@@ -2,13 +2,27 @@ package myactivity.cp16.com.sotral_way.Views.Etudiant;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 import myactivity.cp16.com.sotral_way.R;
 
@@ -19,6 +33,15 @@ import myactivity.cp16.com.sotral_way.R;
 public class EtudiantRecord extends Fragment {
 
     Button registerEtu;
+    EditText nom;
+    EditText prenom;
+    EditText numcard;
+    EditText email;
+    EditText password;
+
+    FirebaseAuth auth;
+    FirebaseDatabase mdatabase;
+    DatabaseReference mreference;
 
     public static final String TAG = "AdministrateurFragment";
 
@@ -28,11 +51,29 @@ public class EtudiantRecord extends Fragment {
         // Inflate the layout for this fragment
 
         View view = inflater.inflate(R.layout.fragment_etudiant_record, container, false);
+
         registerEtu = (Button) view.findViewById(R.id.idBntRegisterEtu);
+        password =  view.findViewById(R.id.input8);
+        email = view.findViewById(R.id.input7);
+        numcard= view.findViewById(R.id.input6);
+        nom= view.findViewById(R.id.input4);
+        prenom = view.findViewById(R.id.input5);
+
+
+
         View.OnClickListener registerEtuOnClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                RegisterEtuOnClick();
+
+                String textpassword =  password.getText().toString();
+                String  textemail = email.getText().toString();
+                String  textnumcard= numcard.getText().toString();
+                String  textnom= nom.getText().toString();
+                String  textprenom = prenom.getText().toString();
+
+                SendtoFirebase(textnom , textprenom,textemail ,textpassword ,textnumcard );
+
+
             }
         };
        registerEtu.setOnClickListener(registerEtuOnClickListener);
@@ -41,6 +82,7 @@ public class EtudiantRecord extends Fragment {
 
     public void RegisterEtuOnClick() {
         EtudiantAuthentification fragment = new EtudiantAuthentification();
+
         if (fragment != null) {
             FragmentManager fragmentManager = getFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -49,5 +91,48 @@ public class EtudiantRecord extends Fragment {
 
         }
 
+    }
+    private void SendtoFirebase(final String nom , final String prenom , final String email , final String password, final String numcard  )
+    {
+       if(TextUtils.isEmpty(nom) ||  TextUtils.isEmpty(prenom) || TextUtils.isEmpty(email) || TextUtils.isEmpty(password) || TextUtils.isEmpty(numcard) )
+       {
+           Toast.makeText(getContext(),"champs non rensegné",Toast.LENGTH_SHORT).show();
+       }
+       else if(password.length()<6)
+       {
+           Toast.makeText(getContext(),"mot de passe doit contenir plus de 6 caractere",Toast.LENGTH_SHORT).show();
+       }
+       else{
+           auth = FirebaseAuth.getInstance();
+           auth.createUserWithEmailAndPassword(email , password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+               @Override
+               public void onComplete(@NonNull Task<AuthResult> task) {
+                   if (task.isSuccessful())
+                   {
+                       FirebaseUser user = auth.getCurrentUser();
+                       String userid = user.getUid();
+                       mreference = FirebaseDatabase.getInstance().getReference("Etudiants");
+                       HashMap<String,  String> etudiant = new HashMap<>();
+                       etudiant.put("nom", nom);
+                       etudiant.put("prenom",prenom);
+                       etudiant.put("email", email);
+                       etudiant.put("password", password);
+                       etudiant.put("numcard", numcard);
+                       mreference.setValue(etudiant).addOnCompleteListener(new OnCompleteListener<Void>() {
+                           @Override
+                           public void onComplete(@NonNull Task<Void> task) {
+                               Toast.makeText(getContext(),"donnees enregistrer avec succes ",Toast.LENGTH_SHORT).show();
+
+                               RegisterEtuOnClick();
+                           }
+                       });
+
+
+                   }
+
+               }
+           });
+
+       }
     }
 }
