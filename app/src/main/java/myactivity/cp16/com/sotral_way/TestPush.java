b31858fe49db4bf64a376b96d363fb1e1d3729b6
shab <alaitoathenas@gmail.com>
2019-02-27 12:06:33 +0000

    wedlastcommint

---
+++ app/src/main/java/myactivity/cp16/com/sotral_way/TestPush.java
@@ -0,0 +1,13 @@
+package myactivity.cp16.com.sotral_way;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestPush extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_push);
+    }
+}
